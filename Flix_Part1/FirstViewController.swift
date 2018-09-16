f7f64efefef598e6e5ea1e2893cc0fe59e22af2b
Vellie <laguerre.vellie@gmail.com>
2018-09-16 10:34:21 -0400

    Initial Commit

---
+++ Flix_Part1/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Flix_Part1
+//
+//  Created by MacBookPro9 on 9/16/18.
+//  Copyright © 2018 MacBookPro9. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
