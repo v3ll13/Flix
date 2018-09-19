ede954e40d48fccfed8c1334a8be8f69cd7d1730
Vellie <laguerre.vellie@gmail.com>
2018-09-19 14:05:53 -0400

    verification of internet connection added

--- Flix_Part1/NowPlayingViewController.swift
+++ Flix_Part1/NowPlayingViewController.swift
@@ -7,10 +7,42 @@
 
 import UIKit
 import AlamofireImage
+import Foundation
+import SystemConfiguration
 
 class NowPlayingViewController: UIViewController , UITableViewDataSource{
     
     //-----------------------------------METHODS------------------------------------
+    
+    func isInternetAvailable() -> Bool
+    {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout.size(ofValue: zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        
+        let defaultRouteReachability = withUnsafePointer(to: &zeroAddress) {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {zeroSockAddress in
+                SCNetworkReachabilityCreateWithAddress(nil, zeroSockAddress)
+            }
+        }
+        
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        return (isReachable && !needsConnection)
+    }
+    
+    func showAlert() {
+        if !isInternetAvailable() {
+            let alert = UIAlertController(title: "Warning", message: "The Internet connection appears to be offline.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Dismiss", style: .default, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+    }
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -50,8 +82,12 @@ class NowPlayingViewController: UIViewController , UITableViewDataSource{
             
             // This will run when the network request returns
             if let error = error {
+                //if self.movies == nil{
+                  //  self.acIndicatorView.startAnimating()
+                //}
                 print(error.localizedDescription)
             } else if let data = data {
+                
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
                 
@@ -60,13 +96,18 @@ class NowPlayingViewController: UIViewController , UITableViewDataSource{
                 self.movies = movies
                 
                 
+                //self.acIndicatorView.stopAnimating()
+                
                 // TODO: Store the movies in a property to use elsewhere
                 
                 // TODO: Reload your table view data
                 self.movieTableView.reloadData()
                 
-                //stop the refresh
+                //-------------stop the refresh----------
                 self.refreshControl.endRefreshing()
+                
+                //stop animating
+                self.acIndicatorView?.stopAnimating()
                 
             }
         }
@@ -82,7 +123,9 @@ class NowPlayingViewController: UIViewController , UITableViewDataSource{
     
     
     
-    //------------Declaring variables and outlets-----------
+    //---------------Declaring variables and outlets-----------------
+   
+    @IBOutlet weak var acIndicatorView: UIActivityIndicatorView!
     @IBOutlet weak var movieTableView: UITableView!
     var movies: [[String: Any]] = []
     var refreshControl: UIRefreshControl!
@@ -107,6 +150,11 @@ class NowPlayingViewController: UIViewController , UITableViewDataSource{
         //-----------calling method fetchMoivies---------
         fetchMovies()
         
+         //----------------Start the activity indicator------------
+         acIndicatorView.startAnimating()
+        
+        //----call for the internet connection
+        showAlert()
     }
     
     
